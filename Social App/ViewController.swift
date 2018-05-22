7818238b9c34a82455d44dc0a7d1d47ad9c5a3ea
William Chen <chenyuwilliam@gmail.com>
2018-05-22 01:02:14 -0400

    Initial Commit

---
+++ Social App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Social App
+//
+//  Created by William Chen on 2018/5/22.
+//  Copyright © 2018 William Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
